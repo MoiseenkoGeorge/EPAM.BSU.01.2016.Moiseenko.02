26fba57e774abdf58b842ef62ff7b4c418a22fcb
MoiseenkoGeorge <moiseenkogeorge1995@gmail.com>
2016-03-14 15:46:13 +0300

    Task 4 done

---
+++ Task4.Tests/CustomerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace Task4.Tests
+{
+    [TestFixture]
+    public class CustomerTests
+    {
+        [Test]
+        [TestCase("Jeffrey Richter", "+1 (425) 555-0100",100000,"Customer record: {0}, {2}, {1}",Result = "Customer record: Jeffrey Richter, 100000, +1 (425) 555-0100")]
+        [TestCase("Jeffrey Richter", "+1 (425) 555-0100", 100000, "Customer record: {1}", Result = "Customer record: +1 (425) 555-0100")]
+        [TestCase("Jeffrey Richter", "+1 (425) 555-0100", 100000, null, ExpectedException = typeof(ArgumentNullException))]
+        [TestCase("Jeffrey Richter", "+1 (425) 555-0100", 100000, "Customer record: {0}, {2}", Result = "Customer record: Jeffrey Richter, 100000")]
+        public string ToString_Test(string name,string contactPhone,decimal revenue,string format)
+        {
+            Customer customer = new Customer(name, contactPhone, revenue);
+            return customer.ToString(format);
+        }
+    }
+}
--- Task4/Customer.cs
+++ Task4/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Task4
 {
-    public class Customer
+    public class Customer : IFormattable
     {
         public string Name { get; private set; }
         public string ContactPhone { get; private set; }
@@ -17,6 +18,19 @@ namespace Task4
             this.Name = name;
             this.ContactPhone = contactPhone;
             this.Revenue = revenue;
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (format == null)
+                throw new ArgumentNullException(nameof(format), "Require not null argument");
+            if (formatProvider == null)
+                formatProvider = CultureInfo.CurrentCulture;
+            return string.Format(formatProvider, format, Name, ContactPhone, Revenue); // {0} - Name, {1} - ContactPhone, {2} - Revenue
+        }
+        public string ToString(string format)
+        {
+            return this.ToString(format, CultureInfo.CurrentCulture);
         }
     }
 }
